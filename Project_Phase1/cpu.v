e21887cfdf80f3c04d04158ffc116852878ef549
finncrux <32180435+finncrux@users.noreply.github.com>
2019-10-13 18:14:43 -0500

    Create Memory System

--- Project_Phase1/cpu.v
+++ Project_Phase1/cpu.v
@@ -4,5 +4,9 @@ input clk, rst_n;
 output hlt;
 output [15:0]pc;
 
-
+// memory system
+wire[15:0]PC_IN,PC_OUT,DATA_IN,DATA_OUT,PC_ADDR,DATA_ADDR;
+wire PC_WE,DATA_WE,PC_RD,DATA_RD;       // write enable when WE and RD are both high
+memory1c InstructionMem (.data_out(PC_OUT), .data_in(PC_IN), .addr(PC_ADDR), .enable(PC_RD), .wr(PC_WE), .clk(clk), .rst(!rst_n));
+memory1c DataMem (.data_out(DATA_OUT), .data_in(DATA_IN), .addr(DATA_ADDR), .enable(DATA_RD), .wr(DATA_WE), .clk(clk), .rst(!rst_n));
 endmodule
