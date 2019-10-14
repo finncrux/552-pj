ec4ea42f9124d9137ca3ef719dd0e90b1a294c91
finncrux <32180435+finncrux@users.noreply.github.com>
2019-10-14 18:13:13 -0500

    Added ALU module

--- Project_Phase1/cpu.v
+++ Project_Phase1/cpu.v
@@ -1,12 +1,19 @@
 module cpu(clk, rst_n, hlt, pc);
-
+// TODO: change initial load image in both memory.
 input clk, rst_n;
 output hlt;
 output [15:0]pc;
 
+wire[3:0] OPOCODE;//opocode
 // memory system
 wire[15:0]PC_IN,PC_OUT,DATA_IN,DATA_OUT,PC_ADDR,DATA_ADDR;
-wire PC_WE,DATA_WE,PC_RD,DATA_RD;       // write enable when WE and RD are both high
+
+wire PC_WE,PC_RD,DATA_WE,DATA_RD;       // write enable when WE and RD are both high
+assign PC_RD = 1'b1;
+assign PC_WE = 1'b0;
+assign pc = PC_OUT;
+assign DATA_WE = DATA_RD&OPOCODE[0];
+assign DATA_RD = OPOCODE[3]&!OPOCODE[2]&!OPOCODE[1];
 memory_I InstructionMem (.data_out(PC_OUT), .data_in(PC_IN), .addr(PC_ADDR), .enable(PC_RD), .wr(PC_WE), .clk(clk), .rst(!rst_n));
-memory_D DataMem (.data_out(DATA_OUT), .data_in(DATA_IN), .addr(DATA_ADDR), .enable(DATA_RD), .wr(DATA_WE), .clk(clk), .rst(!rst_n));
+memory_D DataMed        (.data_out(DATA_OUT), .data_in(DATA_IN), .addr(DATA_ADDR), .enable(DATA_RD), .wr(DATA_WE), .clk(clk), .rst(!rst_n));
 endmodule
