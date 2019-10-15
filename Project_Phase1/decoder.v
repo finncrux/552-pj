4d757e9c7ccaa8e4f2196da6d784e000927c4500
alexdh12 <wangcx1998@126.com>
2019-10-15 14:04:34 -0500

    1

--- Project_Phase1/decoder.v
+++ Project_Phase1/decoder.v
@@ -19,7 +19,7 @@ assign rs = (opcode[3]&(!opcode[2])) ? instruction[7 :4] : instruction[7:4];
 assign rt = (opcode[3]&(!opcode[2])) ? instruction[11:8] : instruction[3:0];
 assign offset_4bit = instruction[3:0];
 assign immediate_8bit = instruction[7:0];
-assign offset_9bit = (opcode == 4'b1100)? instruction[8:0] : 8'h00;
+assign offset_9bit = (opcode == 4'b1100)? instruction[8:0] : 9'h000;
 assign condition = instruction[11:9];
 
 
