88031d375b4efa5f96d0fcf2b629a986e66951f6
alexdh12 <wangcx1998@126.com>
2019-10-15 21:49:01 -0500

    1

--- Project_Phase1/PC.v
+++ Project_Phase1/PC.v
@@ -2,15 +2,15 @@ module PC(input clk, input rst,
             input[2:0] Condition, input[8:0] Imm, input [2:0] Flag, 
             output [15:0] PC_addr, output[15:0] PCS);
 
-wire [15:0] PC_out,dead_line;
-wire rd_en, wt_en, dead;
+wire [15:0] PC_out;
+wire rd_en, wt_en;
 
 assign PCS = PC_out;
 assign rd_en = 1'b1;
 assign wt_en = 1'b1;
 
 PC_control pc_ctrl(.C(Condition),.I(Imm),.F(Flag),.PC_in(PC_addr),.PC_out(PC_out));
-pc_reg pcreg(.rst(rst), .clk(clk), .PC_in(PC_in), .PC_out(PC_out));
+pc_reg pcreg(.rst(rst), .clk(clk), .PC_in(PC_out),.PC_out(PC_addr));
 
 
 endmodule
