aa6e932f6cfbde9746cfa7416e866b0b83490b5c
finncrux <32180435+finncrux@users.noreply.github.com>
2019-10-16 13:34:36 -0500

    Update BitCell.v

--- Project_Phase1/BitCell.v
+++ Project_Phase1/BitCell.v
@@ -1,7 +1,8 @@
 module BitCell( input clk,  input rst, input D, input WriteEnable, input ReadEnable1, input ReadEnable2, inout Bitline1, inout Bitline2);
 wire Q,Qout;
 dff DFF(.q(Q), .d(D), .wen(WriteEnable), .clk(clk), .rst(rst));
-assign Qout = WriteEnable?Q:Q;
+assign Qout = Q;
+//assign Qout = WriteEnable?D:Q;
 bufif1 buf1(Bitline1,Qout,ReadEnable1);
 bufif1 buf2(Bitline2,Qout,ReadEnable2);
 endmodule
