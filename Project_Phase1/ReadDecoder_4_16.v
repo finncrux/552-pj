4b13fe38b24e864446a5975c740706eb201c146d
davidhedong9779 <dhe28@wisc.edu>
2019-10-15 20:25:05 -0500

    decoders

--- Project_Phase1/ReadDecoder_4_16.v
+++ Project_Phase1/ReadDecoder_4_16.v
@@ -1,3 +1,12 @@
-module ReadDecoder_4_16(input [3:0] RegId, output [15:0] Wordline);
-Shifter shft(.Shift_Out(Wordline), .Shift_In(16'h0001), .Shift_Val(RegId), .Mode(1'b0));
+module ReadDecoder_4_16(input[3:0] RegId, output [15:0] Wordline);
+
+wire [15:0] wordline_in0, wordline_in1, wordline_in2;
+
+//shift the 1 if needed
+assign wordline_in = 16'h0001;
+assign wordline_in0 = RegId[0] ? (wordline_in << 1) : wordline_in;
+assign wordline_in1 = RegId[1] ? (wordline_in0 << 2) : wordline_in0;
+assign wordline_in2 = RegId[2] ? (wordline_in1 << 4) : wordline_in1;
+assign Wordline = RegId[3] ? (wordline_in2 << 8) : wordline_in2;
+
 endmodule
--- Project_Phase1/WriteDecoder_4_16.v
+++ Project_Phase1/WriteDecoder_4_16.v
@@ -1,5 +1,14 @@
-module WriteDecoder_4_16(input [3:0] RegId, input WriteReg, output [15:0] Wordline);
-wire[15:0] ShiftResult;
-Shifter shft(.Shift_Out(ShiftResult), .Shift_In(16'h0001), .Shift_Val(RegId), .Mode(1'b0));
-assign Wordline = WriteReg?ShiftResult:16'h0000;
+module WriteDecoder_4_16(input[3:0] RegId, input WriteReg, output[15:0] Wordline);
+
+wire [15:0] wordline_in, wordline_out, wordline_in0, wordline_in1, wordline_in2;
+
+assign Wordline = WriteReg ? wordline_out : 16'h0000;
+
+//shift the 1 if needed
+assign wordline_in = 16'h0001;
+assign wordline_in0 = RegId[0] ? (wordline_in << 1) : wordline_in;
+assign wordline_in1 = RegId[1] ? (wordline_in0 << 2) : wordline_in0;
+assign wordline_in2 = RegId[2] ? (wordline_in1 << 4) : wordline_in1;
+assign wordline_out = RegId[3] ? (wordline_in2 << 8) : wordline_in2;
+
 endmodule
