df8c2a85fd9d89f8e1016a5a2076ba1c253f5ac1
davidhedong9779 <dhe28@wisc.edu>
2019-10-16 22:13:05 -0500

    Update memory_I.v

--- Project_Phase1/memory_I.v
+++ Project_Phase1/memory_I.v
@@ -52,7 +52,7 @@ module memory_I (data_out, data_in, addr, enable, wr, clk, rst);
       if (rst) begin
          //load loadfile_all.img
          if (!loaded) begin
-            $readmemh("test3.txt", mem);
+            $readmemh("test2.txt", mem);
             loaded = 1;
          end
           
