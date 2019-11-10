03d67763464cb040b46588b5319e79eebd87b44d
finncrux <32180435+finncrux@users.noreply.github.com>
2019-11-10 13:41:12 -0600

    Update cpu.v

--- Verilog_File/cpu.v
+++ Verilog_File/cpu.v
@@ -1,5 +1,7 @@
 module cpu(clk, rst_n, hlt, pc);
-
+input clk, rst_n;
+output hlt;
+output[15:0] pc;
 ////////////////////////////////////////////
 //IF////////////////////////////////////////
 ////////////////////////////////////////////
@@ -32,14 +34,14 @@ wire RsMemFwd,RsExFwd;      // RS forwarding?
 wire RtMemFwd,RtExFwd;      // Rt forwarding?
 wire [15:0] RES;            // ALU result
 wire [15:0] IMM;            // 16 bit immediate input
-wire [3:0] opocode;         // operation to execuate
+wire [3:0] OPOCODE;         // operation to execuate
 
 // alu module
-wire [15:0] A,B,RES;
+wire [15:0] A,B;
 assign A = RsMemFwd?MemFwdSource:RsExFwd?ExFwdSource:Rs_Data;
 assign B = RtMemFwd?MemFwdSource:RtExFwd?ExFwdSource:RtExFwd;
-wire OVFL;
-ALU alu(.A(A),.B(B),.I(IMM[7:0]),.RES(RES),.opocode(opocode),.OVFL(OVFL));    
+wire ALU_OVFL;
+ALU alu(.A(A),.B(B),.I(IMM[7:0]),.RES(RES),.opocode(OPOCODE),.OVFL(ALU_OVFL));    
 
 // Flag logic
 assign FlagFromAlu = {(RES==0),(ALU_OVFL),(RES[15]==1)};
@@ -69,5 +71,17 @@ Register_3 FLAGREG(.Q(FLAG),.D(FlagFromAlu),.clk(clk),.rst(!rst_n),.WriteEnableN
 ////////////////////////////////////////////
 
 
+////////////////////////////////////////////
+// FORWARDING UNIT /////////////////////////
+////////////////////////////////////////////
 
+// I/O exposed
+wire [3:0] ID_EX_Rs,  ID_EX_Rt,          // Input: which reg does each stage needs
+           EX_MEM_Rs, EX_MEM_Rt, EX_MEM_Rd;
+wire ID_EX_Rs_EX_Fwd , ID_EX_Rt_EX_Fwd ;   // Output: EX take input from EX Fwd
+wire ID_EX_Rs_MEM_Fwd , ID_EX_Rt_MEM_Fwd;  // Output: EX take input from MEM Fwd
+wire EX_MEM_Rs_Fwd, EX_MEM_Rt_Fwd;        // Output: MEM take input from MEM Fwd
+wire EX_MEM_Opocode_Vld, MEM_WB_Opocode_Vld;     // Input: whether the operation produce useful output in ALU.
+                                          // If the operation is a load or save then the address is not useful.
+assign ID_EX_Rs_Fwd = (ID_EX_Rs!=0) & (EX_MEM_Rd == ID_EX_Rs) & (EX_MEM_Opocode_Vld);
 endmodule
