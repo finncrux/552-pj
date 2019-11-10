479aea120b0c464fa932409f63aa2d5e899dee4d
alexdh12 <wangcx1998@126.com>
2019-11-10 14:56:19 -0600

    Update decode_stage.v

--- Verilog_File/decode_stage.v
+++ Verilog_File/decode_stage.v
@@ -15,12 +15,18 @@ input [15:0] REG_DATA;
 input rst_n, clk;
 input writer_en;
 
+wire Stall, writeReg1_en_ID, writeMem1_en_ID, MEM_DATA_RD_EN1_ID;
+wire [3:0]OPCODE1;
+
 assign MemToReg_ID = !(OPOCODE == 4'b1000);
 assign sign_extend_ID = {{8{I[7]}}, I};
 assign MEM_DATA_RD_EN_ID = OPOCODE[3]&!OPOCODE[2]&!OPOCODE[1];
-
-decoder decoder(.instruction(register_input), .opcode(OPOCODE), .rs(rs), .rt(rt), .rd(rd), 
-.immediate_8bit(I), .offset_9bit(offset_9bit), .condition(CONDITION), .writem_en(writeMem_en_ID), .writer_en(writeReg_en_ID), .halt(hlt));
+assign writeReg_en_ID = Stall? 1'b0 : writeReg1_en_ID;
+assign writeMem_en_ID = Stall? 1'b0 : writeMem1_en_ID;
+assign MEM_DATA_RD_EN_ID = Stall? 1'b0 : MEM_DATA_RD_EN1_ID;
+assign OPOCODE = Stall? 1'b0 : OPCODE1;
+decoder decoder(.instruction(register_input), .opcode(OPOCODE1), .rs(rs), .rt(rt), .rd(rd), 
+.immediate_8bit(I), .offset_9bit(offset_9bit), .condition(CONDITION), .writem_en(writeMem1_en_ID), .writer_en(writeReg1_en_ID), .halt(hlt));
 
 // register file
 RegisterFile regfile(.clk(clk), .rst(!rst_n), .SrcReg1(rs), .SrcReg2(rt), 
