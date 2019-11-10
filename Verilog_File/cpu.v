37735e1bbfde71854e76eb73c76fe09f7933f93c
davidhedong9779 <dhe28@wisc.edu>
2019-11-10 16:28:26 -0600

    hazard detect

--- Verilog_File/cpu.v
+++ Verilog_File/cpu.v
@@ -242,22 +242,5 @@ assign EX_MEM_Rt_Fwd = (EX_MEM_Rt!=0) & (MEM_WB_Rd == EX_MEM_Rt) & (MEM_WB_Opoco
 // HAZARD DETECTION //////////////////////// OK
 ////////////////////////////////////////////
 
-// Detect load to use stall only!!! 
-// The Stall Signal is passed to the ID/EX stage!!!
-// I/O exposed
-wire[3:0] ID_EX_opocode, EX_MEM_opocode;            // Input: Operation on each stage
-wire[3:0] EX_MEM_RD;                                // Input: Load destination
-wire[3:0] ID_EX_RS,ID_EX_RT;                        // Input: the regs that may need the newly loaded data
-wire Stall;                                         // Output: whether the load-to-use stall is needed
-// I/O End
-wire ID_EX_RT_NOIMMEDIATA;                          // Whether RT is actually needed
-wire ID_EX_RT_NOFORWARDING;                         // Whether RT can't be passed in later stage
-assign ID_EX_RT_USED =                              // Not Shift related or PC related instruction
-                ID_EX_opocode[3:2]!=2'b11 & !((ID_EX_opocode[3:2]==2'b01)&(ID_EX_opocode!=4'b0111));
-assign ID_EX_RT_NOFORWARDING=
-                ID_EX_opocode!= 4'b1001;            // if we are storing here, no stall need since we can get the data by forwarding.
-assign Stall =  ((ID_EX_opocode == 4'b1100)|(ID_EX_opocode == 4'b1101))|// B or BR
-                ((EX_MEM_opocode == 4'b1000)         // the memstage is storing
-                &((ID_EX_RS == EX_MEM_RD)|((ID_EX_RT_NOFORWARDING & ID_EX_RT_USED)&
-                (ID_EX_RT == EX_MEM_RD))));          // RT is actually used and no forwarding here.
+
 endmodule
