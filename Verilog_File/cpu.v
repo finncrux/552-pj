2b3020f6d8a7c701b8fcc1fa42a05e83ff55200a
finncrux <32180435+finncrux@users.noreply.github.com>
2019-11-10 16:10:01 -0600

    Update cpu.v

--- Verilog_File/cpu.v
+++ Verilog_File/cpu.v
@@ -242,7 +242,8 @@ assign ID_EX_RT_USED =                              // Not Shift related or PC r
                 ID_EX_opocode[3:2]!=2'b11 & !((ID_EX_opocode[3:2]==2'b01)&(ID_EX_opocode!=4'b0111));
 assign ID_EX_RT_NOFORWARDING=
                 ID_EX_opocode!= 4'b1001;            // if we are storing here, no stall need since we can get the data by forwarding.
-assign Stall = (EX_MEM_opocode == 4'b1000)          // the memstage is storing
+assign Stall =  ((ID_EX_opocode == 4'b1100)|(ID_EX_opocode == 4'b1101))|// B or BR
+                ((EX_MEM_opocode == 4'b1000)         // the memstage is storing
                 &((ID_EX_RS == EX_MEM_RD)|((ID_EX_RT_NOFORWARDING & ID_EX_RT_USED)&
-                (ID_EX_RT == EX_MEM_RD)));          // RT is actually used and no forwarding here.
+                (ID_EX_RT == EX_MEM_RD))));          // RT is actually used and no forwarding here.
 endmodule
